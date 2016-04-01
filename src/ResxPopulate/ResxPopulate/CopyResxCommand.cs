de588c519c560d35a20b09b5a14cec912308eaa4
nikita ivanov <ni.ivanov@2gis.ru>
2016-04-01 18:36:18 +0600

    commit basic structure

---
+++ src/ResxPopulate/ResxPopulate/CopyResxCommand.cs
@@ -0,0 +1,48 @@
+﻿using System.Management.Automation;
+
+namespace GrafGenerator.ResxPopulate
+{
+    [Cmdlet(VerbsCommon.Copy, "Resx", DefaultParameterSetName = "DefaultParameterSet")]
+    public class CopyResxCommand: PSCmdlet
+    {
+        private string _path;
+        private string[] _cultures;
+
+        #region Parameters
+
+        [Parameter(Position = 0, ParameterSetName = "DefaultParameterSet", Mandatory = true)]
+        [ValidateNotNullOrEmpty]
+        public string Path
+        {
+            get { return _path; }
+            set { _path = value; }
+        }
+
+
+        [Parameter(Position = 1, ParameterSetName = "DefaultParameterSet", Mandatory = true)]
+        [ValidateNotNullOrEmpty]
+        public string[] Cultures
+        {
+            get { return _cultures; }
+            set { _cultures = value; }
+        }
+
+        #endregion
+
+
+        #region Overrides
+
+        protected override void BeginProcessing()
+        {
+            
+        }
+
+        protected override void ProcessRecord()
+        {
+            WriteVerbose("Processing file: " + Path);
+        }
+
+        #endregion
+
+    }
+}

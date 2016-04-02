34dc29b781e20ddbcd819591dfd4463dd4fae27a
Nikita Ivanov <grafgenerator@gmail.com>
2016-04-02 16:17:51 +0600

    installation script

--- build/build-and-deploy.ps1
+++ build/build-and-deploy.ps1
@@ -1,10 +1,15 @@
 Set-StrictMode -Version Latest
+Clear
+#=============================
+
 
 Import-Module -Name "$PSScriptRoot\Invoke-MsBuild.psm1" -DisableNameChecking
 
 $moduleName = 'GrafGenerator.ResxPopulate'
 $docsFolder = [Environment]::GetFolderPath('MyDocuments')
 $installFolder = "$docsFolder\WindowsPowerShell\Modules\$moduleName"
+$modulePath = "$installFolder\$moduleName.dll"
+$manifestPath = "$installFolder\$moduleName.psd1"
 
 $rootFolder = "$PSScriptRoot\.."
 $artifactsFolder = "$rootFolder\artifacts"
@@ -26,6 +31,19 @@ else
 
 Write-Host "Install module."
 
-#Copy-Item
+#try to create required folders first
+New-Item -ItemType File -Path $modulePath -Force | Out-Null
 
-Write-Host $installFolder
+Copy-Item -Path "$artifactsFolder\$moduleName.dll" -Destination $modulePath -Force | Out-Null
+Copy-Item -Path "$artifactsFolder\$moduleName.psd1" -Destination $manifestPath -Force | Out-Null
+
+$dllExist = Test-Path $modulePath
+$psdExist = Test-Path $manifestPath
+
+if($dllExist -eq $false -or $psdExist -eq $false){
+    Write-Host "Installation failed. Module not installed."
+}
+else {
+    Write-Host "Installation succeeded. Module installed."
+    Write-Host "Module path: $modulePath"
+}
